61d6b545e0ffdcc4c804efec234b710e1915ace0
Onur Gumus <onur@outlook.com.tr>
2023-06-19 18:22:39 +0000

    add pizzas

--- src/Server/Views/Index.fs
+++ src/Server/Views/Index.fs
@@ -13,10 +13,23 @@ let view (env:#_) (dataLevel: int) = task{
     let serializedToppings = Encode.Auto.toString (toppings, extra = extraThoth)
     let li = 
         pizzas 
-        |> List.map (fun pizza -> html $"""<li>{pizza.Name}</li>""")
+        |> List.map (fun pizza -> 
+        html $"""
+            <li  style="background-image: url('/assets/{pizza.ImageUrl}')">
+            <div class="pizza-info">
+                <span class=title>{pizza.Name}</span>
+                {pizza.Description}
+                <span class=price>{pizza.FormattedBasePrice}</span>
+            </div>
+    </li>
+        """)
         |> String.concat "\r\n"
     return
         html $""" 
-            <fps-pizza-menu toppings='{serializedToppings}'>{li}</fps-pizza-menu>
+            <fps-pizza-menu toppings='{serializedToppings}'>
+                <ul class="pizza-cards">
+                {li}
+                </ul>
+            </fps-pizza-menu>
         """
 }
