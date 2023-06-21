d846a9c75e1052298a641c029348dd977670a74d
Onur Gumus <onur@outlook.com.tr>
2023-06-21 15:42:05 +0000

    sidebar styling

--- src/Client/Sidebar.fs
+++ src/Client/Sidebar.fs
@@ -33,13 +33,51 @@ let view (host:LitElement) (model:Model) dispatch =
     document.addEventListener (Events.PizzaOrdered, handleOrderedPizza) |> ignore
 
     Hook.createDisposable (fun () -> document.removeEventListener (Events.PizzaOrdered, handleOrderedPizza)))
+    if model.Pizzas.Length = 0 then
+        html $"""
+        <div class= "empty-cart">
+            Choose a pizza <br/>
+            to get started
+        </div>
+        """
+    else
+        let cartItem index (pizza: Pizza) =
+            let toppings  =
+                pizza.Toppings 
+                    |> Lit.mapiUnique 
+                        (fun (t:Topping )-> t.Name) 
+                        (fun index (t: Topping) ->html $"""<li>+{t.Name}</li>""")
+            html $"""
+                <div class="cart-item">
+                    <button class="delete-topping">x</button>
+                    <div class="title">
+                        {pizza.Size.ToString()} { pizza.Special.Name }
+                    </div>
+                    <ul>
+                        {toppings}
+                    </ul>
+                    <div class="item-price">
+                        { pizza.FormattedTotalPrice }
+                    </div>
+                </div>
+            """
+        let cartItems = model.Pizzas |> Lit.mapiUnique (fun (p:Pizza) -> p.Id.ToString()) cartItem
 
-    html $"""
-     <div class= "empty-cart">
-         Choose a pizza <br/>
-         to get started
-    </div>
-    """
+        html $"""
+            <div class = "order-contents">
+                <h2> Your order </h2>
+                {cartItems}
+            </div>
+            <div class="order-total">
+                Total:
+                <span class= "total-price">
+                        { model.Pizzas |> List.sumBy (fun p -> Math.Round(p.TotalPrice,2)) |> string }
+                </span>
+                <button class="btn btn-warning">Order ></button>
+            </div>
+         """
+                   
+                
 
 [<LitElement("fps-side-bar")>]
 let LitElement () =
