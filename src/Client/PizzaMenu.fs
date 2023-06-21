b356b6c80f9463afdc139b0ad194b587dff18bf8
Onur Gumus <onur@outlook.com.tr>
2023-06-21 00:38:22 +0000

    formatting

--- src/Client/PizzaMenu.fs
+++ src/Client/PizzaMenu.fs
@@ -27,106 +27,119 @@ let rec execute (host: LitElement) order (dispatch: Msg -> unit) =
     | Order.NoOrder -> ()
 
 [<HookComponent>]
-let view host (model:Model) dispatch =
-    Hook.useEffectOnce (fun () -> 
-        let handleSelectedPizza (e:Event) =
+let view host (model: Model) dispatch =
+    Hook.useEffectOnce (fun () ->
+        let handleSelectedPizza (e: Event) =
             let customEvent = e :?> CustomEvent
             let pizzaSpecial = customEvent.detail :?> Pizza.PizzaSpecial
             let pizza = Pizza.CreatePizzaFromSpecial pizzaSpecial
             dispatch (PizzaSelected pizza)
 
-        host?addEventListener(Events.PizzaSelected, handleSelectedPizza) |> ignore
+        host?addEventListener (Events.PizzaSelected, handleSelectedPizza) |> ignore
 
-        Hook.createDisposable (fun () ->host?removeEventListener (Events.PizzaSelected, handleSelectedPizza))
-    )
-    let topicOption (index:int) (topping:Topping)  = 
-        html $"""
-            <option value={ index }> { topping.Name } - { topping.FormattedBasePrice }</option>
+        Hook.createDisposable (fun () -> host?removeEventListener (Events.PizzaSelected, handleSelectedPizza)))
+
+    let topicOption (index: int) (topping: Topping) =
+        html
+            $"""
+            <option value={index}> {topping.Name} - {topping.FormattedBasePrice}</option>
         """
-            
+
     let toppings =
-        let toppingOptions = model.Toppings |> Lit.mapiUnique (fun t -> t.Id.ToString()) topicOption
-        html $"""
-            <select class="custom-select" @input={Ev(fun e -> dispatch (ToppingAdded(e.target?value))) }>
+        let toppingOptions =
+            model.Toppings |> Lit.mapiUnique (fun t -> t.Id.ToString()) topicOption
+
+        html
+            $"""
+            <select class="custom-select" @input={Ev(fun e -> dispatch (ToppingAdded(e.target?value)))}>
                 <option value="-1">Choose a topping</option>
                 {toppingOptions}
             </select>
         """
-    let toppingInstance (index:int) (topping:Topping) = 
-        html $"""
+
+    let toppingInstance (index: int) (topping: Topping) =
+        html
+            $"""
             <div class="topping">
-                <span class="topping-name">{ topping.Name }</span>
-                <span class="topping-price">{ topping.FormattedBasePrice }</span>
+                <span class="topping-name">{topping.Name}</span>
+                <span class="topping-price">{topping.FormattedBasePrice}</span>
                 <button  class="delete-topping" 
                     @click={Ev(fun _ -> dispatch (ToppingRemoved topping))}>x</button>
             </div>
         """
+
     let toppingInstances =
         match model.Pizza with
-        | Some pizza -> 
-            let toppings = pizza.Toppings |> Lit.mapiUnique (fun t -> t.Id.ToString()) toppingInstance
-            html $"""
+        | Some pizza ->
+            let toppings =
+                pizza.Toppings |> Lit.mapiUnique (fun t -> t.Id.ToString()) toppingInstance
+
+            html
+                $"""
                 <div class="toppings">
-                    { toppings }
+                    {toppings}
                 </div>
             """
         | None -> Lit.nothing
 
-    let dialog (pizza:Pizza.Pizza) = 
-        html $"""
+    let dialog (pizza: Pizza.Pizza) =
+        html
+            $"""
         <div class="dialog-container">
             <div class="dialog">
                 <div class="dialog-title">
-                    <h2> { pizza.Special.Name } </h2>
-                    { pizza.Special.Description }
+                    <h2> {pizza.Special.Name} </h2>
+                    {pizza.Special.Description}
                 </div>
                 <form class="dialog-body">
                     <div>
                         <label>Toppings:</label>
-                        { toppings }
+                        {toppings}
                     </div>
-                    { toppingInstances}
+                    {toppingInstances}
                     <div>
                         <label>Size:</label>
-                        <input type="range" .value={ pizza.Size } min={ Pizza.MinimumSize }
-                            max= { Pizza.MaximumSize } step="1" @input={ Ev (fun e -> dispatch (SizeChanged e.target?value) )}  />
+                        <input type="range" .value={pizza.Size} min={Pizza.MinimumSize}
+                            max= {Pizza.MaximumSize} step="1" @input={Ev(fun e -> dispatch (SizeChanged e.target?value))}  />
                         <span class="size-label">
-                            { pizza.Size }" (£{ pizza.FormattedTotalPrice })
+                            {pizza.Size}" (£{pizza.FormattedTotalPrice})
                         </span>
                     </div>
                 </form>
                 <div class="dialog-buttons">
                     <button id="cancelButton" class="btn btn-secondary mr-auto" @click={Ev(fun _ -> dispatch PizzaCancelled)}>Cancel</button>
                     <span class="mr-center">
-                        Price: <span class="price">{ pizza.FormattedTotalPrice }</span>
+                        Price: <span class="price">{pizza.FormattedTotalPrice}</span>
                     </span>
                     <button id="confirmButton" class="btn btn-success ml-auto" @click={Ev(fun _ -> dispatch PizzaConfirmed)}>Order ></button>
                 </div>
             </div>
         </div>
         """
+
     match model.Pizza with
     | Some pizza -> dialog pizza
-    | None ->
-        Lit.nothing
-       
+    | None -> Lit.nothing
 
 [<LitElement("fps-pizza-menu")>]
 let LitElement () =
     Hook.useHmr (hmr)
-    let host, prop = LitElement.init (fun config -> 
-        let split (str: string): Topping list =
-           let res = Decode.Auto.fromString<Topping list>(str, extra = extraEncoders)
-           match res with
-              | Ok x -> x
+
+    let host, prop =
+        LitElement.init (fun config ->
+            let split (str: string) : Topping list =
+                let res = Decode.Auto.fromString<Topping list> (str, extra = extraEncoders)
+
+                match res with
+                | Ok x -> x
                 | Error x -> []
 
-        config.useShadowDom <- false
-        config.props <-
-        {|
-            toppings = Prop.Of([], attribute="toppings", fromAttribute = split)
-        |}
-    )
+            config.useShadowDom <- false
+
+            config.props <- {|
+                toppings = Prop.Of([], attribute = "toppings", fromAttribute = split)
+            |})
+
     let program =
         Program.mkHiddenProgramWithOrderExecute (init (prop.toppings.Value)) (update) (execute host)
 #if DEBUG
