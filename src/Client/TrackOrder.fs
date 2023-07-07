cd4bcb8e2415f3cd694ad7f4a75e03306611f426
Onur Gumus <onur@outlook.com.tr>
2023-07-07 11:03:46 +0000

    Fix leak

--- src/Client/TrackOrder.fs
+++ src/Client/TrackOrder.fs
@@ -24,21 +24,16 @@ open Elmish.Bridge
 open FunPizzaShop.Domain.API
 open TrackOrder
 
+#if DEBUG
 let private hmr = HMR.createToken ()
+#endif
 
-let rec execute (host: LitElement) (bridgeSend: ClientToServer.Msg ->unit) order (dispatch: Msg -> unit) =
+let rec execute (host: LitElement) order (dispatch: Msg -> unit) =
     match order with
     | Order.NoOrder -> ()
     | Order.TrackOrder orderId ->
-        bridgeSend (ClientToServer.Msg.TrackOrder orderId)
-
-[<HookComponent>]
-let view (host:LitElement) (model:Model) dispatch =
-    html $"""
-        <div class='main'>
-            { model.Order}
-        </div>
-    """
+        Bridge.NamedSend("TrackOrder", (ClientToServer.Msg.TrackOrder orderId))
+    
 let mapClientMsg msg =
     match msg with
     | _ -> Remote msg
@@ -46,17 +41,39 @@ let mapClientMsg msg =
 let bc = 
     Bridge.endpoint 
         endpoint 
-        |>  Bridge.withUrlMode UrlMode.Replace 
-        |>  Bridge.withMapping mapClientMsg 
+        |> Bridge.withUrlMode UrlMode.Replace 
+        |> Bridge.withMapping mapClientMsg 
+        |> Bridge.withName "TrackOrder"
+
+[<HookComponent>]
+let view (host:LitElement) (model:Model) dispatch =
+    Hook.useEffectOnce (fun () -> 
+        Hook.createDisposable(fun () ->  
+        (bc :> IDisposable).Dispose()
+        match Elmish.Bridge.Helpers.mappings.Value with
+        | Some map ->  
+            Elmish.Bridge.Helpers.mappings.Value <- Some (map.Remove("TrackOrder" |> Some))
+        | None -> ())
+    )
+    html $"""
+        <div class='main'>
+            { model.Order}
+        </div>
+    """
+
 
 [<LitElement("fps-trackorder")>]
 let LitElement () =
+
+#if DEBUG
     Hook.useHmr (hmr)
+#endif
+
     let host, _ = LitElement.init (fun config -> 
         config.useShadowDom <- false
     )
     let program =
-        Program.mkHiddenProgramWithOrderExecute (init) (update) (execute host Bridge.Send)
+        Program.mkHiddenProgramWithOrderExecute (init) (update) (execute host)
         |> Program.withBridgeConfig bc
 #if DEBUG
         |> Program.withDebugger
